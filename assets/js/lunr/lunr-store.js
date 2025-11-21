31cc9f682fd7944f385c3aeab8a04857fe663c25
alexlancaster <alexlancaster@users.noreply.github.com>
2025-11-21 08:27:13 +0000

    Deploy RIIS 2.0 site to production: ronininstitute.org

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [,{
+    "title": "Events",
+    "excerpt":"    RIIS 2.0 Public Events Calendar     (note default timezone is shown below is US Pacific Time)         By joining a RIIS 2.0 Public Events, you agree to the RIIS 2.0 Code of Conduct    ","url": "/events/"
+  },{
+    "title": "Frequently Asked Questions",
+    "excerpt":"This FAQ contains answers to some common questions. It is a simplified guide. In case of any conflict, the RIIS 2.0 Bylaws and adopted policies (Affiliation Terms of Service, Code of Conduct, Belonging Policy, Privacy Policy, Conflict Resolution Policy) govern. About us What is RIIS 2.0? RIIS 2.0 is a...","url": "/faq/"
+  },{
+    "title": "RIIS 2.0 Fellow Application Form",
+    "excerpt":"               Loading…             ","url": "/fellow-application-form/"
+  },{
+    "title": "Ronin Institute for Independent Scholarship 2.0",
+    "excerpt":"At RIIS 2 0, we believe that knowledge flourishes when barriers fall. Our community brings together scholars from across disciplines — scientists, artists, writers, and innovators—who share a commitment to truth, empathy, and belonging. Sign-up to our news updates to follow our progress as we co-create new pathways for scholarly...","url": "/"
+  },{
+    "title": "Updates",
+    "excerpt":"        ","url": "/updates/"
+  }]
