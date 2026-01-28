4086613f70e0fb9cb032a6db9aa6453f588a245a
alexlancaster <alexlancaster@users.noreply.github.com>
2026-01-28 04:39:03 +0000

    Deploy RIIS 2.0 site to production: ronininstitute.org

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [,{
+    "title": "Events",
+    "excerpt":"        By joining a RIIS 2.0 Public Events, you agree to the RIIS 2.0 Code of Conduct    ","url": "/events/"
+  },{
+    "title": "Frequently Asked Questions",
+    "excerpt":"This FAQ contains answers to some common questions. It is a simplified guide. In case of any conflict, the RIIS 2.0 Bylaws and adopted policies govern. About us What is RIIS 2.0? RIIS 2.0 is a California nonprofit public benefit corporation organized under IRS Section 501(c)(3). Its mission is to support...","url": "/faq/"
+  },{
+    "title": "RIIS 2.0 Fellow Application Form",
+    "excerpt":"               Loading…             ","url": "/fellow-application-form/"
+  },{
+    "title": "Ronin Institute for Independent Scholarship 2.0",
+    "excerpt":"At RIIS 2 0, we believe that knowledge flourishes when barriers fall. Our community brings together scholars from across disciplines — scientists, artists, writers, and innovators—who share a commitment to truth, empathy, and belonging. Sign-up to our news updates to follow our progress as we co-create new pathways for scholarly...","url": "/"
+  },{
+    "title": "Fellows",
+    "excerpt":"Fellows Sort order: random alphabetical Filter by keyword 🗙 Clear 👤 Mohammad Alanzi Semiconductor physics and photonics More... Keywords: Semiconductors, applied physics, thin films coating, Photonics 👤 Lawrence (Larry) Andrews crystallography, esp. lattices and inorganic chemistry More... Keywords: crystallography, chemistry 👤 Kimberly Beek Buddhist Studies and Religion and Literature More......","url": "/scholars/"
+  },{
+    "title": "Updates",
+    "excerpt":"        ","url": "/updates/"
+  }]
